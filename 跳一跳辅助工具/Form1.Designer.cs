f547a107eb494f6e1d6773a26a2502ab2def705e
嘿咯world <478894154@qq.com>
2018-01-06 15:00:33 +0800

    ver:1.0

---
+++ 跳一跳辅助工具/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace 跳一跳辅助工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelY2 = new System.Windows.Forms.Label();
+            this.labelX2 = new System.Windows.Forms.Label();
+            this.labelY1 = new System.Windows.Forms.Label();
+            this.labelX1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelY2);
+            this.panel1.Controls.Add(this.labelX2);
+            this.panel1.Controls.Add(this.labelY1);
+            this.panel1.Controls.Add(this.labelX1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(209, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(205, 281);
+            this.panel1.TabIndex = 1;
+            // 
+            // labelY2
+            // 
+            this.labelY2.AutoSize = true;
+            this.labelY2.Location = new System.Drawing.Point(121, 191);
+            this.labelY2.Name = "labelY2";
+            this.labelY2.Size = new System.Drawing.Size(0, 12);
+            this.labelY2.TabIndex = 9;
+            // 
+            // labelX2
+            // 
+            this.labelX2.AutoSize = true;
+            this.labelX2.Location = new System.Drawing.Point(34, 191);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(0, 12);
+            this.labelX2.TabIndex = 8;
+            // 
+            // labelY1
+            // 
+            this.labelY1.AutoSize = true;
+            this.labelY1.Location = new System.Drawing.Point(121, 80);
+            this.labelY1.Name = "labelY1";
+            this.labelY1.Size = new System.Drawing.Size(0, 12);
+            this.labelY1.TabIndex = 7;
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.Location = new System.Drawing.Point(34, 81);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(0, 12);
+            this.labelX1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(91, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Y：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "X：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Y：";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Point2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Point1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "跳一跳";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 360);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "跳一跳";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelY2;
+        private System.Windows.Forms.Label labelX2;
+        private System.Windows.Forms.Label labelY1;
+        private System.Windows.Forms.Label labelX1;
+    }
+}
+
---
+++ 跳一跳辅助工具/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 跳一跳辅助工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int AdbExec(String cmd)
+        {
+            Process compiler = new Process();
+            compiler.StartInfo.FileName = ".\\adb\\adb.exe";
+            compiler.StartInfo.Arguments = cmd;
+            compiler.StartInfo.UseShellExecute = false;
+            compiler.StartInfo.CreateNoWindow = true;
+            compiler.Start();
+            compiler.WaitForExit();
+            int ret = compiler.ExitCode;
+            return ret;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(AdbExec("shell touch /sdcard/test_connect") != 0)
+            {
+                MessageBox.Show("Adb连接失败，请安装Android驱动");
+                Application.Exit();
+            }
+            AdbExec("shell screencap -p /sdcard/screencap.png");
+            AdbExec("pull /sdcard/screencap.png .\\config\\screencap.png");
+            System.Drawing.Image img = Image.FromFile(".\\config\\screencap.png");
+            System.Drawing.Image bmp = new System.Drawing.Bitmap(img);
+            img.Dispose();
+            pictureBox1.Image = bmp;
+        }
+
+        static Boolean firstFlag = true;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mouse = (MouseEventArgs)e;
+            int x = mouse.X;
+            int y = mouse.Y;
+            if (firstFlag)
+            {
+                firstFlag = !firstFlag;
+                labelX1.Text = x.ToString();
+                labelY1.Text = y.ToString();
+            }else
+            {
+                firstFlag = !firstFlag;
+                labelX2.Text = x.ToString();
+                labelY2.Text = y.ToString();
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(labelX1.Text == ""||labelX2.Text == "")
+            {
+                return;
+            }
+            int x1, y1, x2, y2;
+            x1 = int.Parse(labelX1.Text);
+            y1 = int.Parse(labelY1.Text);
+            x2 = int.Parse(labelX2.Text);
+            y2 = int.Parse(labelY2.Text);
+            int width = x2 - x1;
+            int height = y2 - y1;
+            double distance = Math.Sqrt(width*width+height*height);
+            double value = 8.65;
+            int touchTime = (int)(distance * value);
+            AdbExec("shell input swipe 250 250 251 251 "+touchTime.ToString());
+
+            //MessageBox.Show("distance:"+distance.ToString()+"touchTime:"+touchTime.ToString());
+            Thread.Sleep(1000);
+
+            AdbExec("shell screencap -p /sdcard/screencap.png");
+            AdbExec("pull /sdcard/screencap.png .\\config\\screencap.png");
+            System.Drawing.Image img = System.Drawing.Image.FromFile(".\\config\\screencap.png");
+            System.Drawing.Image bmp = new System.Drawing.Bitmap(img);
+            img.Dispose();
+            pictureBox1.Image = bmp;
+        }
+    }
+}
